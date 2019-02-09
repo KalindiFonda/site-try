bfb52748b09781f956b23cbd2409be2dc07a823e
Kalindi Fonda <kalindi.fonda@gmail.com>
2019-02-09 08:57:13 -0800

    just something

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ $(".text").each("mouseover", function() {
   $(".text").addClass('permahover');
 });
 
+
+$(function() {
+    $(window).on('scroll', function() {
+        $('.img').css('margin-top', $(window).scrollTop() * -.3);
+    });
+});
